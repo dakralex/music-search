60a425e0e47ab734be3b9e999c39e7cdc4dd178d
Daniel Kral <dakral@protonmail.com>
2023-05-08 20:37:35 +0200

    Fix recordSearchValue to remove last item when list is full

--- src/features/search/searchSlice.ts
+++ src/features/search/searchSlice.ts
@@ -1,5 +1,7 @@
 import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 
+const MAX_RECENT_SEARCH_ITEMS = 5;
+
 export interface SearchState {
   recentSearchValues: Array<string>;
 }
@@ -17,14 +19,19 @@ export const searchSlice = createSlice({
         searchValue.toLowerCase().replace(/\s/g, '');
       const generalSearchTerm = getGeneralTerm(action.payload);
 
+      // Remove items that are similar to the given search value
       state.recentSearchValues = state.recentSearchValues.filter(
         (searchValue: string) =>
           getGeneralTerm(searchValue) !== generalSearchTerm,
       );
 
-      state.recentSearchValues.push(action.payload);
+      // Remove first item from recent search values if it exceeds limit
+      if (state.recentSearchValues.length > MAX_RECENT_SEARCH_ITEMS - 1) {
+        state.recentSearchValues.splice(0, 1);
+      }
 
-      state.recentSearchValues.splice(5);
+      // Add new search item to recent search values
+      state.recentSearchValues.push(action.payload);
     },
   },
 });
